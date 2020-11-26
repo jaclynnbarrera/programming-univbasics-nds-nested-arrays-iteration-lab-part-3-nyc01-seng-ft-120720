568a58e42240184df280f51ae1c470a95843fb6b
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-26 01:43:43 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,15 @@ guessing_game_grid = [
   [15,16,99,1,2,11]
   ]
 
+total = 0
+row_index = 0 
+
+while row_index < guessing_game_grid do
+  element_index = 0
+  while element_index < guessing_game_grid[row_index].count do
+    total += guessing_game_grid[row_index][element_index]
+    element
+end 
 
 
 def join_nested_strings(src)
