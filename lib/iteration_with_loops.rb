27cff5c297a732cded3982f0b1b2bbba1125477f
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-26 02:42:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,19 +1,17 @@
 
-
-def join_nested_strings(arr)
-  final_string = " "
+def join_nested_strings(src)
+  total = []
   row_index = 0
-  while row_index < arr.count do
+  while row_index < src.count do
     element_index = 0
-    while element_index < arr[row_index].count do
-      
-      if arr[row_index][element_index].class == String
-        final_string + arr[row_index][element_index]
-        element_index += 1
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].class == String
+        total << src[row_index][element_index]
       end
-    row_index += 1
+      element_index += 1
     end
-end
-puts final_string
+    row_index += 1
+  end
+  total.join(" ")
 end
 
